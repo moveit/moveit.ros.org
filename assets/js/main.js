a58caa1edb5324f2570a40c121f81c2ca9032aa1
Ladislav Skvarka <lskvarka@blueberryapps.com>
2019-03-04 17:07:26 +0100

    Live JS time-line

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,39 @@ $('#modalVideo').on('hidden.bs.modal', function(e) {
   // stop the video
   $('#modalVideo iframe').attr('src', null);
 });
+
+function currentVersion() {
+  var currentDate = Date.now();
+  var startDate = new Date('05/23/2018');
+  var finalDate = new Date('04/01/2019');
+  var totalTime = finalDate.getTime() - startDate.getTime();
+  var progressTime = currentDate - startDate;
+  var percent = (progressTime / totalTime) * 100;
+  if (currentDate >= finalDate.getTime()) {
+    $('#time-line--red-1').css('display', 'block');
+  } else {
+    $('#time-line--orange-1').css('width', `${percent}%`);
+  }
+}
+
+function futureVersion() {
+  var currentDate = Date.now();
+  var startDate = new Date('02/01/2019');
+  var finalDate = new Date('12/01/2019');
+  var freezeDate = new Date('11/01/2019');
+  var totalTime = finalDate.getTime() - startDate.getTime();
+  var progressTime = currentDate - startDate;
+  var percent = (progressTime / totalTime) * 100;
+  if (currentDate >= finalDate.getTime()) {
+    $('#time-line--red-2').css('display', 'block');
+  } else if (currentDate > freezeDate.getTime()) {
+    $('#time-line--orange-2').css('width', `${percent}%`);
+  } else {
+    $('#time-line--blue-2').css('width', `${percent}%`);
+  }
+}
+
+$(document).ready(function() {
+  currentVersion();
+  futureVersion();
+});
