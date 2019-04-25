9f6a3d1b06a13100b01ce95036185bd7b5df0561
Dave Coleman <davetcoleman@gmail.com>
2019-04-24 19:44:44 -0600

    Improve build_locally.sh to have travis argument

--- build_locally.sh
+++ build_locally.sh
@@ -10,6 +10,16 @@ if [[ "$0" != "${BASH_SOURCE}" ]]; then
   return 1
 fi
 
+have_travis_arg() {
+  # Check for existance of command line argument
+  for arg in "$@"; do
+    if [[ "${arg}" == "travis" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
 # Install dependencies
 sudo apt-get install ruby ruby-dev build-essential
 export GEM_HOME=$HOME/gems
@@ -18,11 +28,22 @@ gem install jekyll
 gem install bundler -v 1.17.3
 bundle install
 
-# Build website using similar script as Travis, but skip htmlproofer because it fails locally:
-bundle exec jekyll build --strict_front_matter
+# Optionally test website using same script as Travis.
+# When you are developing locally, you may or may not want to run the full set of tests that Travis does
+if have_travis_arg "$@"; then
+  ./.travis.sh
+fi
 
 # Launch website
-echo "Opening http://localhost:4000 in default browser"
+echo
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo "Preparing to open http://localhost:4000"
+echo "Be sure to refresh auto-opened webpage"
+read -p "Press any key to start serving website..."
+echo "-------------------------------------"
+echo "-------------------------------------"
+echo
 xdg-open http://localhost:4000
 
 # Start serving website locally
