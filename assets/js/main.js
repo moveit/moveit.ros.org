0a2f1bbb7271772526b8b7f8ffe45e78ce6851af
Dave Coleman <davetcoleman@gmail.com>
2020-02-27 07:45:50 -0800

    Roadmap for MoveIt 2.1 Foxy

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,10 @@ $('#modalVideo').on('hidden.bs.modal', function(e) {
 
 function currentVersion() {
   var currentDate = Date.now();
-  var startDate  = new Date('03/01/2019');
-  var freezeDate = new Date('04/01/2020');
-  var finalDate  = new Date('05/01/2020');
+  //                        DAY / MONTH / YEAR
+  var startDate  = new Date('03/01/2019'); // Start Date
+  var freezeDate = new Date('04/01/2020'); // Feature Freeze
+  var finalDate  = new Date('05/01/2020'); // Release Date
   var totalTime = finalDate.getTime() - startDate.getTime();
   var progressTime = currentDate - startDate;
   var percent = (progressTime / totalTime) * 100;
@@ -36,10 +37,11 @@ function currentVersion() {
 }
 
 function futureVersion() {
-  var currentDate = Date.now();
-  var startDate  = new Date('06/01/2019'); // Start Date
-  var freezeDate = new Date('02/01/2020'); // Feature Freeze
-  var finalDate  = new Date('02/14/2020'); // Release Date
+    var currentDate = Date.now();
+  //                        DAY / MONTH / YEAR
+  var startDate  = new Date('02/14/2020'); // Start Date
+  var freezeDate = new Date('06/01/2020'); // Feature Freeze
+  var finalDate  = new Date('07/01/2020'); // Release Date
   var totalTime = finalDate.getTime() - startDate.getTime();
   var progressTime = currentDate - startDate;
   var percent = (progressTime / totalTime) * 100;
