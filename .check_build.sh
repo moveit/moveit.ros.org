7452eb27c62a98cd7444f14ca21d305e3fdd397a
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-10-18 21:55:04 +0200

    htmlproofer: consider moveit.ros.org as an internal domain

--- .check_build.sh
+++ .check_build.sh
@@ -2,4 +2,4 @@
 
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
 bundle exec jekyll build --strict_front_matter
-bundle exec htmlproofer ./_site --check-html --only-4xx --http-status-ignore 429 --alt-ignore '/.*/'
+bundle exec htmlproofer ./_site --check-html --only-4xx --http-status-ignore 429 --internal-domains moveit.ros.org --alt-ignore '/.*/'
