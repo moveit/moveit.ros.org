874d29ec9a8e1feda3e23d6aa89b77c5c95e7920
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-10-18 22:20:22 +0200

    htmlproofer: more thorough check

--- .check_build.sh
+++ .check_build.sh
@@ -3,3 +3,6 @@
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
 bundle exec jekyll build --strict_front_matter
 bundle exec htmlproofer ./_site --check-html --only-4xx --http-status-ignore 429 --internal-domains moveit.ros.org --alt-ignore '/.*/'
+
+# Perform a more thorough check
+bundle exec htmlproofer ./_site --check-html --http-status-ignore 429,503 --internal-domains moveit.ros.org --alt-ignore '/.*/' || true
